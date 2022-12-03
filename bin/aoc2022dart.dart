c4c79151175475421c63a0bb193eb87c4f4c3b2b
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2022-12-03 13:10:01 +0100

    Spelling

--- bin/aoc2022dart.dart
+++ bin/aoc2022dart.dart
@@ -5,16 +5,16 @@ Future<void> main() async {
   final resultDay1Part1 = await Day1.runPart1();
   final resultDay1Part2 = await Day1.runPart2();
 
-  print('Max calories carried by an elf: $resultDay1Part1');
-  print('Total calories carried by top 3 elves: $resultDay1Part2');
+  print('Max calories carried by an Elf: $resultDay1Part1');
+  print('Total calories carried by top 3 Elves: $resultDay1Part2');
   print('--- END OF DAY 1 ---\n');
 
   print('--- START OF DAY 2 ---');
   final resultDay2Part1 = await Day2.runPart1();
   final resultDay2Part2 = await Day2.runPart2();
 
-  print('RPS score: $resultDay2Part1');
-  print('Rigged RPS score: $resultDay2Part2');
+  print('Rock-Paper-Scissors score: $resultDay2Part1');
+  print('Rigged Rock-Paper-Scissors score: $resultDay2Part2');
   print('--- END OF DAY 2 ---\n');
 
   print('--- START OF DAY 3 ---');
